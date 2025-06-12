1afa5f50b6f95f56b5274ee733f3c320265ea4cc
vrushteej <vrushteej.04@gmail.com>
2025-06-12 13:17:52 +0530

    mongo uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const uri = 'mongodb://localhost:27017/new';
 
-mongoose.connect(uri, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
